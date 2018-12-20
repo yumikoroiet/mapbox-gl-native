f1d6c9fc746053dd2c47d9ce91926ee79a774b32
Lloyd Sheng <i@lloydsheng.com>
2018-12-20 11:34:40 +0800

    fix iOS 11 issue

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -1054,9 +1054,9 @@ public:
                                        constant:size.height]];
     }
 
-    [containerView removeConstraints:constraints];
+    [NSLayoutConstraint deactivateConstraints:constraints];
     [constraints removeAllObjects];
-    [containerView addConstraints:updatedConstrants];
+    [NSLayoutConstraint activateConstraints:updatedConstrants];
     [constraints addObjectsFromArray:updatedConstrants];
 }
 
